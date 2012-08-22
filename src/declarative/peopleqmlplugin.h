07c7683baef244ca8d8f74e28be5cb3ee2990193
Aleix Pol <aleixpol@kde.org>
2012-08-22 19:03:47 +0200

    Expose the model in a declarative plugin

---
+++ src/declarative/peopleqmlplugin.h
@@ -0,0 +1,32 @@
+/*
+    Persons Model
+    Copyright (C) 2012  Aleix Pol Gonzalez <aleixpol@blue-systems.com>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef PEOPLEQMLPLUGIN_H
+#define PEOPLEQMLPLUGIN_H
+
+#include <QDeclarativeExtensionPlugin>
+
+class PeopleQMLPlugin : public QDeclarativeExtensionPlugin
+{
+    virtual void registerTypes(const char* uri);
+};
+
+Q_EXPORT_PLUGIN2(kpeopledeclarative, PeopleQMLPlugin);
+
+#endif // PEOPLEQMLPLUGIN_H

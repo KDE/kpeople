7ec2833a62e294846b10db2900b58cb30f0847d0
Aleix Pol <aleixpol@kde.org>
2012-08-24 17:07:42 +0200

    add information in the lovelypeople delegate

--- src/examples/lovelypeople.cpp
+++ src/examples/lovelypeople.cpp
@@ -27,8 +27,12 @@ class ContactDelegate : public QStyledItemDelegate
 {
     virtual void paint(QPainter* painter, const QStyleOptionViewItem& option, const QModelIndex& index) const {
         QStyledItemDelegate::paint(painter, option, index);
-        if(index.parent().isValid())
-            painter->drawText(option.rect.center(), index.data(PersonsModel::IMRole).toString());
+        QRect infoRect(QPoint(option.rect.center().x(), option.rect.top()), option.rect.bottomRight());
+        if(index.parent().isValid()) {
+            painter->drawText(infoRect, index.data(PersonsModel::IMRole).toString());
+        } else {
+            painter->drawText(infoRect, QString::number(index.model()->rowCount(index)));
+        }
     }
 };
 

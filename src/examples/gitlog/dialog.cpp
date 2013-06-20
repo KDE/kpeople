e3f4a17247facce4b75fcf23570ed6064aefbca8
David Edmundson <kde@davidedmundson.co.uk>
2013-06-20 01:26:12 +0100

    Add actions to git log example

--- src/examples/gitlog/dialog.cpp
+++ src/examples/gitlog/dialog.cpp
@@ -1,6 +1,9 @@
 #include "dialog.h"
 #include "logmodel.h"
+#include <persondata.h>
+#include <personpluginmanager.h>
 #include "ui_dialog.h"
+#include <QMenu>
 
 Dialog::Dialog(QWidget *parent) :
     QDialog(parent),
@@ -8,9 +11,30 @@ Dialog::Dialog(QWidget *parent) :
 {
     ui->setupUi(this);
     ui->tableView->setModel(new LogModel(this));
+    ui->tableView->setContextMenuPolicy(Qt::CustomContextMenu);
+    connect(ui->tableView, SIGNAL(customContextMenuRequested(QPoint)), SLOT(onCustomContextMenuRequested(QPoint)));
 }
 
 Dialog::~Dialog()
 {
     delete ui;
+}
+
+void Dialog::onCustomContextMenuRequested(const QPoint& point)
+{
+    const QModelIndex &index = ui->tableView->indexAt(point);
+    if (!index.isValid()) {
+        return;
+    }
+
+    const QString &emailAddress = index.data().toString();
+    PersonData person;
+    person.setContactId(emailAddress);
+
+    QMenu menu;
+    QList<QAction*> actions = PersonPluginManager::actionsForPerson(&person, &menu);
+    if (actions.size()) {
+        menu.addActions(actions);
+        menu.exec(ui->tableView->mapToGlobal(point));
+    }
 }
--- src/examples/gitlog/dialog.h
+++ src/examples/gitlog/dialog.h
@@ -10,11 +10,12 @@ class Dialog;
 class Dialog : public QDialog
 {
     Q_OBJECT
-    
+
 public:
     explicit Dialog(QWidget *parent = 0);
     ~Dialog();
-    
+private Q_SLOTS:
+    void onCustomContextMenuRequested(const QPoint &point);
 private:
     Ui::Dialog *ui;
 };

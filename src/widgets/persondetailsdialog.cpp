22718c63c64516fedf7c010c8d109d7873ee605b
David Edmundson <kde@davidedmundson.co.uk>
2013-07-23 14:07:33 +0000

    Add a dialog wrapping PersonDetailsView

---
+++ src/widgets/persondetailsdialog.cpp
@@ -0,0 +1,67 @@
+/*
+ * Convience dialog for displaying PersonDetailsView widgets
+ * Copyright 2013  David Edmundson <davidedmundson@kde.org>
+ *
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+#include "persondetailsdialog.h"
+#include <QVBoxLayout>
+#include <QDialogButtonBox>
+
+#include <kpeople/widgets/persondetailsview.h>
+
+using namespace KPeople;
+
+namespace KPeople {
+class PersonDetailsDialogPrivate {
+public:
+    PersonDetailsView *view;
+};
+}
+
+PersonDetailsDialog::PersonDetailsDialog(QWidget *parent, Qt::WindowFlags f):
+    QDialog(parent, f),
+    d_ptr(new PersonDetailsDialogPrivate)
+{
+    Q_D(PersonDetailsDialog);
+    QVBoxLayout *layout = new QVBoxLayout(this);
+
+    d->view = new PersonDetailsView(this);
+    layout->addWidget(d->view);
+
+    QDialogButtonBox *buttons = new QDialogButtonBox(this);
+    buttons->setStandardButtons(QDialogButtonBox::Ok);
+    layout->addWidget(buttons);
+
+    connect(buttons, SIGNAL(accepted()), this, SLOT(accept()));
+
+    setLayout(layout);
+}
+
+PersonDetailsDialog::~PersonDetailsDialog()
+{
+    delete d_ptr;
+}
+
+void PersonDetailsDialog::setPerson(const PersonDataPtr &person)
+{
+    Q_D(PersonDetailsDialog);
+    d->view->setPerson(person);
+}
+
+
+#include "persondetailsdialog.moc"
---
+++ src/widgets/persondetailsdialog.h
@@ -0,0 +1,49 @@
+/*
+ * Convience dialog for displaying PersonDetailsView widgets
+ * Copyright 2013  David Edmundson <davidedmundson@kde.org>
+ *
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+#ifndef PERSONDETAILSDIALOG_H
+#define PERSONDETAILSDIALOG_H
+
+#include <QtGui/QDialog>
+
+#include <kpeople_export.h>
+#include <kpeople/persondata.h>
+
+namespace KPeople
+{
+class PersonDetailsDialogPrivate;
+
+class KPEOPLE_EXPORT PersonDetailsDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit PersonDetailsDialog(QWidget *parent=0, Qt::WindowFlags f=0);
+    virtual ~PersonDetailsDialog();
+
+    void setPerson(const PersonDataPtr &person);
+
+private:
+    Q_DECLARE_PRIVATE(PersonDetailsDialog)
+    PersonDetailsDialogPrivate* d_ptr;
+};
+
+}
+#endif // PERSONDETAILSDIALOG_H

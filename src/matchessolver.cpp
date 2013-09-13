1fdd7dd9a12b744984c43e47fd86fa3d3c55b824
Aleix Pol <aleixpol@kde.org>
2013-09-13 03:56:08 +0200

    Fix the matchers resolver in MatchesSolver job
    Properly figure out how do we want to merge the contacts.
    Not closing the bug since the GUI needs a similar fix. The important part
    is fix fixed now, so we won't mess with the database.
    
    CCBUG: 324814

--- src/matchessolver.cpp
+++ src/matchessolver.cpp
@@ -39,17 +39,48 @@ void MatchesSolver::start()
 
 void MatchesSolver::startMatching()
 {
-    //group all matching pairs into a list of URIs indexed by the row of the 'destination' contact
-    QMap<int, QSet<QUrl> > jobsData;
+    //Will contain all the sets to be merged
+    QHash<QUrl, QSet<QUrl> > jobsData;
+    // has a relation of each person, to know where it is
+    QHash<QUrl, QUrl> destinationResolver;
     Q_FOREACH(const Match &m, m_matches) {
-        QPersistentModelIndex idxDestination = m.indexA;
-        QPersistentModelIndex idxOrigin = m.indexB;
-        Q_ASSERT(idxDestination.isValid() && idxOrigin.isValid());
+        QUrl urlA = m.indexA.data(PersonsModel::UriRole).toUrl();
+        QUrl urlB = m.indexB.data(PersonsModel::UriRole).toUrl();
+        Q_ASSERT(urlA != urlB);
 
-        QSet<QUrl>& jobs = jobsData[idxDestination.row()];
-        jobs.insert(idxDestination.data(PersonsModel::UriRole).toUrl());
-        jobs.insert(idxOrigin.data(PersonsModel::UriRole).toUrl());
-        Q_ASSERT(jobs.size()>=2);
+        const bool inA = destinationResolver.contains(urlA);
+        const bool inB = destinationResolver.contains(urlB);
+        if(inA && inB) {
+            //Both are in already, so we have to merge both sets
+            destinationResolver[urlB] = urlA;
+            jobsData[urlA] = jobsData.take(urlB);
+
+            //we've put all items pointed to by urlA, to the B set
+            //now we re-assign anything pointing to B as pointing to A
+            //because they are the same
+            QList<QUrl> keys = destinationResolver.keys(urlB);
+            foreach(const QUrl& key, keys) {
+                destinationResolver[key] = urlA;
+            }
+        } else {
+            //if A is in but not B, we want B wherever A is
+            if(inA) {
+                qSwap(urlB, urlA);
+            }
+            //in case B is anywhere, add A to that set, otherwise just insert B
+            const QUrl mergeUrl = destinationResolver.value(urlB, urlB);
+
+            QSet<QUrl>& jobs = jobsData[mergeUrl];
+            jobs.insert(urlB);
+            jobs.insert(urlA);
+
+            //remember where urlA and urlB are
+            Q_ASSERT(urlA != mergeUrl);
+            destinationResolver.insert(urlA, mergeUrl);
+            if (urlB != mergeUrl) {
+                destinationResolver.insert(urlB, mergeUrl);
+            }
+        }
     }
 
     Q_FOREACH(const QSet<QUrl>& uris, jobsData) {

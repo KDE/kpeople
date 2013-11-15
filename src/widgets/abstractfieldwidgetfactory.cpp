55f860c06fc716f9c7d1506c540784e7cb0021ef
David Edmundson <kde@davidedmundson.co.uk>
2013-11-15 01:00:45 +0100

    Add missing file from last commit

---
+++ src/widgets/abstractfieldwidgetfactory.cpp
@@ -0,0 +1,13 @@
+#include "abstractfieldwidgetfactory.h"
+
+using namespace KPeople;
+
+AbstractFieldWidgetFactory::AbstractFieldWidgetFactory(QObject *parent):
+    QObject(parent)
+{
+}
+
+AbstractFieldWidgetFactory::~AbstractFieldWidgetFactory()
+{
+
+}

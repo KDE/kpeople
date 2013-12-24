a198a6a6c32c5b14b714fb899178e0fbcbc84a9b
David Edmundson <kde@davidedmundson.co.uk>
2013-12-24 23:12:50 +0100

    Make sure test setup has finished before trying to run tests

--- src/autotests/personmanagertests.cpp
+++ src/autotests/personmanagertests.cpp
@@ -27,12 +27,24 @@
 
 QTEST_MAIN(PersonManagerTests);
 
-void waitForSignal(QObject* object, const char* signal, int timeout = 500)
+void waitForSignal(QObject* object, const char* signal, int count=1, int timeout = 500)
 {
-    QEventLoop loop;
-    loop.connect(object, signal, SLOT(quit()));
-    QTimer::singleShot(timeout, &loop, SLOT(quit()));
-    loop.exec();
+    for (int i=0; i < count ; i++) {
+        QEventLoop loop;
+        loop.connect(object, signal, SLOT(quit()));
+        QTimer::singleShot(timeout, &loop, SLOT(quit()));
+        loop.exec();
+    }
+}
+
+
+//debug helper, prints out the received signals on a signalspy
+//assumes all our signals have 2 arguments
+void debugSignals(const QSignalSpy &spy)
+{
+    for(int i=0; i< spy.count(); i++) {
+        qDebug() << spy.signal() << spy[i][0] << spy[i][1];
+    }
 }
 
 
@@ -78,9 +90,8 @@ void PersonManagerTests::mergeContacts()
     QCOMPARE(mergedId, QString("kpeople://1"));
 
     //we expect two signals one for each contact added
-    waitForSignal(m_manager, SIGNAL(contactAddedToPerson(QString, QString)));
-    waitForSignal(m_manager, SIGNAL(contactAddedToPerson(QString, QString)));
-    qDebug() << spy;
+    waitForSignal(m_manager, SIGNAL(contactAddedToPerson(QString, QString)), 2);
+    debugSignals(spy);
     QCOMPARE(spy.count(), 2);
 
     //check database updated correctly
@@ -94,18 +105,21 @@ void PersonManagerTests::mergeContactAndPerson()
     //create a person
     m_manager->mergeContacts(QStringList() << "a://contact1" << "a://contact2");
 
+    waitForSignal(m_manager, SIGNAL(contactAddedToPerson(QString, QString)), 2);
+
+
     //when merging a contact to a person we simply add the contact to an existing person
     QSignalSpy spy(m_manager, SIGNAL(contactAddedToPerson(QString, QString)));
     QString mergedId = m_manager->mergeContacts(QStringList() << "kpeople://1" << "a://contact3");
     QCOMPARE(mergedId, QString("kpeople://1"));
 
     waitForSignal(m_manager, SIGNAL(contactAddedToPerson(QString, QString)));
-    qDebug() << spy;
+    debugSignals(spy);
     QCOMPARE(spy.count(), 1);
 
     //check database updated correctly
     QMultiHash<QString, QString> people = m_manager->allPersons();
-    QCOMPARE(people.count("kpeople://1"), 2);
+    QCOMPARE(people.count("kpeople://1"), 3);
     QCOMPARE(people.values("kpeople://1").toSet(), QSet<QString>() << "a://contact1" << "a://contact2" << "a://contact3");
 }
 
@@ -113,6 +127,7 @@ void PersonManagerTests::mergePeople()
 {
     m_manager->mergeContacts(QStringList() << "a://contact1" << "a://contact2");
     m_manager->mergeContacts(QStringList() << "a://contact3" << "a://contact4");
+    waitForSignal(m_manager, SIGNAL(contactAddedToPerson(QString, QString)), 4);
 
     QSignalSpy spy(m_manager, SIGNAL(contactAddedToPerson(QString, QString)));
     QString mergeId = m_manager->mergeContacts(QStringList() << "kpeople://1" << "kpeople://2");
@@ -121,9 +136,8 @@ void PersonManagerTests::mergePeople()
     QCOMPARE(mergeId, QString("kpeople://1"));
 
     //we expect two signals one for each contact added
-    waitForSignal(m_manager, SIGNAL(contactAddedToPerson(QString, QString)));
-    waitForSignal(m_manager, SIGNAL(contactAddedToPerson(QString, QString)));
-    qDebug() << spy;
+    waitForSignal(m_manager, SIGNAL(contactAddedToPerson(QString, QString)), 2);
+    debugSignals(spy);
     QCOMPARE(spy.count(), 2);
 
 
